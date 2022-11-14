3c86d237797f6755d21d9cf33902a705d89d71d0
SCar011 <sc20ko@brocku.ca>
2022-11-13 22:45:33 -0500

    ch3 init

---
+++ lab9/challenge3/script.js
@@ -0,0 +1,13 @@
+var date = new Date();
+year = date.getFullYear();
+
+let age = prompt("What is your age?");
+
+birthyear = year - age;
+printout  = birthyear;
+
+document.write(printout)
+
+console.log(age);
+console.log(year)
+console.log(birthyear)
